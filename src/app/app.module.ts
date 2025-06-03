ba5d3bc1cd79d276501b2af77ca9af8343d4c659
tiziani <lorenzo.tiziani04@gmail.com>
2025-06-03 12:37:05 +0200

    ora le pagine si visualizzano

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CurrencyPipe } from '@angular/common';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './utils/auth.interceptor';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
+    { provide: LOCALE_ID, useValue: 'it-IT' },
+    CurrencyPipe,
+    provideHttpClient(
+      withInterceptors([authInterceptor]),
+    )
+  ],
+  exports:[RegisterComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
